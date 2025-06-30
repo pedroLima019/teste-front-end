71d7764270fecc4580dcca4a14ecc0f3de676581
Pedro <pedroliveiralima21@gmail.com>
2025-06-30 20:32:16 -0300

    feat(category): adicionando seção de categorias

--- teste-front-end/src/App.tsx
+++ teste-front-end/src/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from "./assets/components/Header";
 import { Banner } from "./assets/components/Banner";
+import { Category } from "./assets/components/Category";
 import "./assets/styles/global.scss";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <Banner />
+      <Category />
     </>
   );
 }
--- teste-front-end/src/assets/components/Category.tsx
+++ teste-front-end/src/assets/components/Category.tsx
@@ -1,9 +1,63 @@
 import "../styles/Category.scss";
 
 export const Category = () => {
-  return <section className="category_container">
-        
-  </section>;
+  return (
+    <section className="category_container" aria-label="Categorias de produtos">
+      <ul className="category_list">
+        <li className="banner">
+          <div className="banner_img">
+            <img src="/image/image.png" alt="Categoria de Tecnologia" />
+          </div>
+          <p>Tecnologia</p>
+        </li>
+        <li className="banner">
+          <div className="banner_img">
+            <img
+              src="/image/supermercados 1.png"
+              alt="Categoria de Supermercado"
+            />
+          </div>
+          <p>Supermercado</p>
+        </li>
+        <li className="banner">
+          <div className="banner_img">
+            <img src="/image/whiskey.png" alt="Categoria de Bebidas" />
+          </div>
+          <p>Bebidas</p>
+        </li>
+        <li className="banner">
+          <div className="banner_img">
+            <img
+              src="/image/ferramentas 1.png"
+              alt="Categoria de Ferramentas"
+            />
+          </div>
+          <p>Ferramentas</p>
+        </li>
+        <li className="banner">
+          <div className="banner_img">
+            <img
+              src="/image/cuidados-de-saude 1.png"
+              alt="Categoria de Saúde"
+            />
+          </div>
+          <p>Saúde</p>
+        </li>
+        <li className="banner">
+          <div className="banner_img">
+            <img src="/image/corrida 1.png" alt="Categoria de Esportes" />
+          </div>
+          <p>Esportes</p>
+        </li>
+        <li className="banner">
+          <div className="banner_img">
+            <img src="/image/moda 1.png" alt="Categoria de Moda" />
+          </div>
+          <p>Moda</p>
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default Category;
